2c2111be2b47ec2c9cf8c7705c2d1a681e97a654
Reena-dll <ouz.spy@gmail.com>
2022-07-31 16:18:18 +0300

    31 Temmuz Gün Sonu Commit 1 = Autofac

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>();
+            builder.RegisterType<ColorManager>().As<IColorService>();
+            builder.RegisterType<BrandManager>().As<IBrandService>();
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<CustomerManager>().As<ICustomerService>();
+            builder.RegisterType<RentalManager>().As<IRentalService>();
+
+            builder.RegisterType<EfCarDal>().As<ICarDal>();
+            builder.RegisterType<EfColorDal>().As<IColorDal>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>();
+            builder.RegisterType<EfRentalDal>().As<IRentalDal>();
+            builder.RegisterType<EfBrandDal>().As<IBrandDal>();
+
+
+        }
+    }
+}
